5037e59cd80608663c1bd5b75e06092d7c36e494
Brian Seeders <brian.seeders@elastic.co>
2021-08-12 13:20:10 -0400

    Install kubectl

--- .buildkite/scripts/steps/demo_env/auth.sh
+++ .buildkite/scripts/steps/demo_env/auth.sh
@@ -4,5 +4,9 @@ set -euo pipefail
 
 echo '--- Auth and set up kubectl'
 
+if [[ ! "$(command -v kubectl)" ]]; then
+  gcloud components install kubectl
+fi
+
 gcloud container clusters get-credentials public-kibana --region us-central1 --project elastic-kibana-184716
 kubectl config use-context gke_elastic-kibana-184716_us-central1_public-kibana
